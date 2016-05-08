0d4362c7e63a92a6dd3e6eb7ee6a54f9e7c9a1e6
John Doran(Phorest) <jdoran@phorest.com>
2016-05-08 23:53:02 +0100

    update sample

--- app.js
+++ app.js
@@ -5,43 +5,22 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var request = require('request');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('FB Bot!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
 
 
 app.get('/webhook/', function (req, res) {
